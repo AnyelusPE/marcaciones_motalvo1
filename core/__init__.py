4dfd12d711d0fed2dc82acc801dadcc48c064da5
AnyelusPE <anyellandeomorales@gmail.com>
2025-10-07 12:47:49 -0500

    fix: limpiar __init__.py para evitar imports circulares

--- core/__init__.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import sys, os
-sys.path.append(os.path.dirname(__file__))
-
-from core.auth import verificar_login
-from roles.salon_app import run_salon_app
-from roles.financista_app import run_financista_app
